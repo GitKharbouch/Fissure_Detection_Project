99d7298fc9fffe56ab6dc62c225f786c0f0f7034
GitKharbouch <y.kharbouch@edu.umi.ac.ma>
2023-01-23 12:08:06 +0100

    added critic function and description

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 
+# FUNCTIONS
 
-# templist
-Templist(x,y,w,h) is a function that takes four arguments(Coordinates, width and height) and returns a list(l1) of coordinate lists(lj)
+## templist
+Templist(x,y,w,h) is a function that takes four arguments(Coordinates and dimensions) and returns a list(l1) of coordinate lists(lj)
+
+## critic
+Critic(x,y,w,h,j) is a function that takes five arguments (Coordinates, dimensions and frame number) and returns critique as True if the fissure is considered critic (Rules by a set of conditions) or False if it is not.
---
+++ critic.py
@@ -0,0 +1,21 @@
+count=[]                                            #Variable that keeps track of fissures and frame number
+critique=0
+
+def critic(x,y,w,h,j):
+    global count,critique
+    if w>100 or h>100 or w*h>1500 :   c=True        #Limits will be set depending on conveyer material    
+    else:   c=False
+          
+    if c==True:                                     #critic on two successive frames->critique = true
+        count.append(j)
+        if len(count)>3:
+            if  count[j-1]!=0 and count[j-2] !=0 :
+                critique=True
+        else : critique=False
+
+    elif c==False :
+        count.append(0)
+        critique=False
+        
+    return critique
+
