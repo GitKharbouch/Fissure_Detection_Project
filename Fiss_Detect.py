1350a7e1bf47625af5445e7ed883c71fb120ef8d
GitKharbouch <y.kharbouch@edu.umi.ac.ma>
2023-01-28 19:57:18 +0100

    Solved major errors regarding critic and dataframe

--- Fiss_Detect.py
+++ Fiss_Detect.py
@@ -1,19 +1,20 @@
+
 import torch
 import cv2 as cv
 import pandas as pd
 import critic
 import time
 
-X,Y,W,H,IMG,TIME=[],[],[],[],[],[] #coordinates,dimensions,image id,time of detection
+X,Y,W,H,CONF,IMG,TIME=[],[],[],[],[],[],[] #coordinates,dimensions,image id,time of detection
 i=0
 idimg=0
 
-
+data=pd.DataFrame({'X':X,'Y':Y,'W':W,'H':H,'IMG_ID':IMG,'Confidence':CONF,'TIME':TIME})
 model = torch.hub.load('yolov5-master', 'custom',
 path='yolov5-master\models\MODEL_V3.pt', source='local',force_reload=True)    #loading the custom model
 
 
-cap = cv.VideoCapture(0)
+cap = cv.VideoCapture(r'C:\Users\youss\OneDrive\Pictures\Camera Roll\vid.mp4')
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -27,29 +28,39 @@ while True:
     
     if len(result)!=0:
         for pred in result:
+            
+
             cv.rectangle(frame,(int(pred[0]),int(pred[1])),((int(pred[2])),(int(pred[3]))),(255,0,0),2)
-            cv.imwrite('images/{}.jpg'.format(idimg),frame)
+            
 
-            if len(critic.count)>20 and critic.count[i]==0 :   # if list is long and no fissures are detected
+            if len(critic.count)>20 and critic.count[i-1]==0 :   # if list is long and no fissures are detected
                 critic.count=[]     #empty the list 
+                X,Y,W,H,CONF,IMG,TIME=[],[],[],[],[],[],[]
                 i=0
-
+            
             X.append(pred[0])
             Y.append(pred[1])
             W.append(pred[2])
             H.append(pred[3])
-            IMG.append('images/{}.jpg'.format(idimg))   #storing the img id with coordinates so it can be identified in the image file 
+            CONF.append(pred[4])
+            IMG.append('images/{}.jpg'.format(idimg))   #storing the img id with coordinates so it can be identified in the image file
             TIME.append(time.ctime())
             
-            critic.critic(X[i],Y[i],W[i],H[i],i)
-           
+            critic.critic(X[i],Y[i],W[i],H[i],CONF,i)   #check if critic before storing the data
             
-            #if critic.critique == True:
-             #  data=pd.DataFrame({'X':X,'Y':Y,'W':W,'H':H,'IMG_ID':IMG,'TIME':TIME})
-              # data.to_excel('Fissuredata\Fissure_data.xlsx_data.xlsx')  
+            
+            if critic.critique == True:
+               
+               cv.imwrite('data/images/{}.jpg'.format(idimg),frame)
+               idimg=idimg+1  
+               new_row={'X':X[i],'Y':Y[i],'W':W[i],'H':H[i],'IMG_ID':IMG[i],'Confidence':CONF[i],'TIME':TIME[i]}
+               data=data.append(new_row,ignore_index=True)
+               data.to_excel('data\Fissure_data.xlsx')
+               
                
             i=i+1
-            idimg=idimg+1
+             
+           
             
            
     
--- critic.py
+++ critic.py
@@ -1,19 +1,21 @@
 count=[]                                            #Variable that keeps track of fissures and frame number
-critique=0
+critique=False
 
-def critic(x,y,w,h,j):
+def critic(x,y,w,h,conf,j):
     global count,critique
-    if w>100 or h>100 or w*h>3000 :   c=True        #Limits will be set depending on conveyer material    
+    if w>120 or h>120  :   c=True        #Limits will be set depending on conveyer material    
     else:   c=False
           
-    if c==True:                                     #critic on two successive frames->critique = true
+    if c==True and conf[j]>= 0.5 :                                     #critic on two successive frames->critique = true
+                                                                    #and confidence >= 05
         count.append(j)
         if len(count)>3:
             if  count[j-1]!=0 and count[j-2] !=0 :
                 critique=True
+            else : critique=False
         else : critique=False
 
-    elif c==False :
+    else  :
         count.append(0)
         critique=False
         
